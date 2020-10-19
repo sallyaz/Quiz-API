52d5214e743e26350dd34109927801ce35fc0c2a
sallyaz <sallyazulay@gmail.com>
2020-10-19 14:08:18 +0300

    cors added

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const userResults = require('./users.json');
 const cors = require('cors');
 
 const port = process.env.PORT || 3030;
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
